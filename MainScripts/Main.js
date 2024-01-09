3573c12e7ebb4731addc9db70ef8df4c5640415a
Tom Cogzell <58867012+C0gzy@users.noreply.github.com>
2024-01-09 18:26:30 +0000

    Added next page functionaility

--- MainScripts/Main.js
+++ MainScripts/Main.js
@@ -1,27 +1,39 @@
 var Page = 0;
 var DisplayPerPage = 60;
+var CurrentDisplayedImages = [];
+var Database;
 
 window.onload = function() {
     fetch('/StarterDatabase.json').then(response => response.json()).then(data => {
         console.log(data);
+        Database = data;
+
         UpdatePage();
 
     })
 
 }
 
-function UpdatePage() {
 
-    for (var i=0; i < (Page * 10) + DisplayPerPage; i++){
-        console.log(data[i].primaryTitle);
+
+function UpdatePage() {
+    console.log("Updating Page" + CurrentDisplayedImages);
+    for (var i=0; i < CurrentDisplayedImages.length; i++){
+        CurrentDisplayedImages[i].remove();
+    }
+
+    for (var i=(Page * DisplayPerPage); i < (Page * DisplayPerPage) + DisplayPerPage; i++){
+        console.log(Database[i].primaryTitle);
         var FilmImage = document.createElement("div")
 
-        if ((data[i].PosterImage == null ) || (data[i].PosterImage == "N/A")){
-            FilmImage.innerHTML = `<button class="grid-item"> <img  src=Images/No_picture_available.png > </button>`
+        if ((Database[i].PosterImage == null ) || (Database[i].PosterImage == "N/A")){
+            FilmImage.innerHTML = `<button class="grid-item"> <img class="Grid-Image" src=Images/No_picture_available.png > </button>`
         }else{
-            FilmImage.innerHTML = `<button class="grid-item"> <img  src=${data[i].PosterImage} > </button>`
+            FilmImage.innerHTML = `<button class="grid-item"> <img class="Grid-Image" src=${Database[i].PosterImage} > </button>`
         }
 
-        document.getElementById("FilmGrid").appendChild(FilmImage);     
+        document.getElementById("FilmGrid").appendChild(FilmImage);  
+        CurrentDisplayedImages.push(FilmImage);   
     }
+    Page++;
 }
